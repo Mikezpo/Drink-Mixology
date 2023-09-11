3011730e7f4e02c297e0dd967ed6ae4c507eaccc
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-09-11 17:13:41 +0100

    Updated layout

--- js/main.js
+++ js/main.js
@@ -1,3 +1,38 @@
+// NAVBAR SLIDER
+
+const toggleButton = document.getElementsByClassName('navButton')[0];
+const navbarLinks = document.getElementsByClassName('navLinks')[0];
+
+// Add click event listener to each menu item
+const menuItems = document.querySelectorAll('.menu-list a');
+for (const item of menuItems) {
+  item.addEventListener('click', () => {
+    // Close the menu when an item is clicked
+    navbarLinks.classList.remove('active');
+    toggleButton.checked = false;
+  });
+}
+
+toggleButton.addEventListener('click', (event) => {
+  event.stopPropagation();
+  navbarLinks.classList.toggle('active');
+});
+
+
+// Close the menu when clicking outside of it
+document.addEventListener('click', (event) => {
+  const isClickInsideMenu = navbarLinks.contains(event.target);
+  const isClickOnButton = event.target === toggleButton;
+
+  if (!isClickInsideMenu && !isClickOnButton) {
+    navbarLinks.classList.remove('active');
+    toggleButton.checked = false;
+  }
+});
+
+// API
+
+
 fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=soft_drink`)
     .then(res => res.json())
     .then(data => {
