a32450dc7c8e048593448d026abd3e00f6a9c3b2
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-09-21 13:14:53 +0100

    Updated drinks.html

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ document.addEventListener('click', (event) => {
 // API
 
 
-fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=soft_drink`)
+fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic`)
     .then(res => res.json())
     .then(data => {
         console.log(data.drinks);
