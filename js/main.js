7347062a7b560a3c69bcf09b39cf9210db20c072
Jose Oliveira <josemiguelpinheiro@gmail.com>
2023-11-02 16:15:52 +0000

    Updated main Ingredients API Fetch

--- js/main.js
+++ js/main.js
@@ -114,31 +114,76 @@ document.querySelector('.mainIngredientList').addEventListener('click', function
   }
 });
 
+// Fetch specific ingredient
 
 function getDrinksByIngredient(ingredient) {
-  fetch(`https://www.thecocktaildb.com/api/json/v1/1/random.php?i=${ingredient}`)
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${ingredient}`)
   .then(res => res.json())
   .then(data => {
     console.log(data.drinks);
     const drinksCount = data.drinks.length;
     if (drinksCount > 0) {
       const randomIndex = Math.floor(Math.random() * drinksCount);
-      const randomDrink = data.drinks[randomIndex];
-      
-      document.querySelector('.cocktailName').innerText = randomDrink.strDrink;
-      document.querySelector('.cocktailImage').src = randomDrink.strDrinkThumb;
-      document.querySelector('.cocktailGlass').innerText = randomDrink.strGlass;
-      document.querySelector('.cocktailDescription').innerText = randomDrink.strInstructions;
+      const randomDrinkId = data.drinks[randomIndex].idDrink;
+      getDrinkDetails(randomDrinkId);
     } else {
-      document.querySelector('.cocktailName').innerText = "No drinks found";
-      document.querySelector('.cocktailImage').src = "";
-      document.querySelector('.cocktailDescription').innerText = "";
+      updateUIForNoDrinks();
     }
   })
   .catch(err => {
-    console.log(`Error ${err}`);
+    console.error(`Error: ${err}`);
   });
 }
+
+// Fetch each drink ID
+
+function getDrinkDetails(drinkId) {
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drinkId}`)
+  .then(res => res.json())
+  .then(data => {
+    const drink = data.drinks[0];
+    displayDrinkInfo(drink);
+    displayIngredientsAndMeasurements(drink);
+  })
+  .catch(err => {
+    console.error(`Error: ${err}`);
+  });
+}
+
+// Fetch API info
+
+function displayDrinkInfo(drink) {
+  document.querySelector('.cocktailName').innerText = drink.strDrink;
+  document.querySelector('.cocktailImage').src = drink.strDrinkThumb;
+  document.querySelector('.cocktailGlass').innerText = drink.strGlass;
+  document.querySelector('.cocktailDescription').innerText = drink.strInstructions;
+}
+
+function displayIngredientsAndMeasurements(drink) {
+  const ingredientsList = document.querySelector('.cocktailIngredients');
+  ingredientsList.innerHTML = ''; // Clear existing ingredients
+
+  for (let i = 1; i <= 15; i++) { // Maximum amount of ingredients per cocktail
+    const ingredient = drink[`strIngredient${i}`];
+    const measure = drink[`strMeasure${i}`];
+    if (ingredient) {
+      const listItem = document.createElement('li');
+      listItem.textContent = `${measure ? measure : ''} ${ingredient}`;
+      ingredientsList.appendChild(listItem);
+    }
+  }
+}
+
+// If no info is available
+
+function updateUIForNoDrinks() {
+  document.querySelector('.cocktailName').innerText = 'No drinks found';
+  document.querySelector('.cocktailImage').src = '';
+  document.querySelector('.cocktailGlass').innerText = '';
+  document.querySelector('.cocktailDescription').innerText = '';
+  document.querySelector('.cocktailIngredients').innerHTML = '';
+}
+
 
 //////////////////////// Fetch API for Random Cocktail
 
@@ -265,159 +310,3 @@ function getDrink() {
 // https://www.thecocktaildb.com/api/json/v1/1/list.php?g=list
 // https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list
 // https://www.thecocktaildb.com/api/json/v1/1/list.php?a=list
-
-// Path for each drink name
-
-// dateModified
-// : 
-// "2015-08-18 14:42:59"
-// idDrink
-// : 
-// "11007"
-// strAlcoholic
-// : 
-// "Alcoholic"
-// strCategory
-// : 
-// "Ordinary Drink"
-// strCreativeCommonsConfirmed
-// : 
-// "Yes"
-// strDrink
-// : 
-// "Margarita"
-// strDrinkAlternate
-// : 
-// null
-// strDrinkThumb
-// : 
-// "https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg"
-// strGlass
-// : 
-// "Cocktail glass"
-// strIBA
-// : 
-// "Contemporary Classics"
-// strImageAttribution
-// : 
-// "Cocktailmarler"
-// strImageSource
-// : 
-// "https://commons.wikimedia.org/wiki/File:Klassiche_Margarita.jpg"
-// strIngredient1
-// : 
-// "Tequila"
-// strIngredient2
-// : 
-// "Triple sec"
-// strIngredient3
-// : 
-// "Lime juice"
-// strIngredient4
-// : 
-// "Salt"
-// strIngredient5
-// : 
-// null
-// strIngredient6
-// : 
-// null
-// strIngredient7
-// : 
-// null
-// strIngredient8
-// : 
-// null
-// strIngredient9
-// : 
-// null
-// strIngredient10
-// : 
-// null
-// strIngredient11
-// : 
-// null
-// strIngredient12
-// : 
-// null
-// strIngredient13
-// : 
-// null
-// strIngredient14
-// : 
-// null
-// strIngredient15
-// : 
-// null
-// strInstructions
-// : 
-// "Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass."
-// strInstructionsDE
-// : 
-// "Reiben Sie den Rand des Glases mit der Limettenscheibe, damit das Salz daran haftet. Achten Sie darauf, dass nur der äußere Rand angefeuchtet wird und streuen Sie das Salz darauf. Das Salz sollte sich auf den Lippen des Genießers befinden und niemals in den Cocktail einmischen. Die anderen Zutaten mit Eis schütteln und vorsichtig in das Glas geben."
-// strInstructionsES
-// : 
-// null
-// strInstructionsFR
-// : 
-// null
-// strInstructionsIT
-// : 
-// "Strofina il bordo del bicchiere con la fetta di lime per far aderire il sale.\r\nAvere cura di inumidire solo il bordo esterno e cospargere di sale.\r\nIl sale dovrebbe presentarsi alle labbra del bevitore e non mescolarsi mai al cocktail.\r\nShakerare gli altri ingredienti con ghiaccio, quindi versarli delicatamente nel bicchiere."
-// strInstructionsZH-HANS
-// : 
-// null
-// strInstructionsZH-HANT
-// : 
-// null
-// strMeasure1
-// : 
-// "1 1/2 oz "
-// strMeasure2
-// : 
-// "1/2 oz "
-// strMeasure3
-// : 
-// "1 oz "
-// strMeasure4
-// : 
-// null
-// strMeasure5
-// : 
-// null
-// strMeasure6
-// : 
-// null
-// strMeasure7
-// : 
-// null
-// strMeasure8
-// : 
-// null
-// strMeasure9
-// : 
-// null
-// strMeasure10
-// : 
-// null
-// strMeasure11
-// : 
-// null
-// strMeasure12
-// : 
-// null
-// strMeasure13
-// : 
-// null
-// strMeasure14
-// : 
-// null
-// strMeasure15
-// : 
-// null
-// strTags
-// : 
-// "IBA,ContemporaryClassic"
-// strVideo
-// : 
-// null
